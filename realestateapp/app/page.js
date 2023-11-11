e105caf9ab8505ddae23a96f27ef7c3170b34ca8
arnavbansal172 <89563103+arnavbansal172@users.noreply.github.com>
2023-11-11 22:28:05 +0530

    modified:   realestateapp/app/layout.js 	modified:   realestateapp/app/page.js

--- realestateapp/app/page.js
+++ realestateapp/app/page.js
@@ -1,8 +1,31 @@
 export default function Home() {
   return (
-    // TODO - update home page here
     <main>
-      Hello World
+      <section>
+        <h1>Bansal RealEstate Welcomes you</h1>
+        <p>Properties at your finger trip</p>
+      </section>
+
+      <section>
+        <h2>Explore Properties</h2>
+        <ul>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/Properties">
+              <a>Properties</a>
+            </Link>
+          </li>
+        </ul>
+      </section>
     </main>
-  )
+  );
 }
